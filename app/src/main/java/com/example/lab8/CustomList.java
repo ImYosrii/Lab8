121eb3d4f7c6013916cea01d1cc741157bcf211e
Yosri Swidan <yosrisaed99@gmail.com>
2023-03-09 19:35:12 -0700

    has city test fixed

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -68,6 +68,9 @@ public class CustomList extends ArrayAdapter<City> {
      * @return
      */
     public boolean hasCity(City city){
+        if (cities.contains(city)){
+            return true;
+        }
         return false;
     }
 }
